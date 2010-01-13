06db8fbcc4302f2c1e8db2ef58ee7950ab6204f8
Jay Hannah <jay@jays.net>
2010-01-12 20:29:22 -0600

    Ruby 101

---
+++ check_the_check/Pawn.rb
@@ -0,0 +1,16 @@
+
+class Pawn
+   #attr_reader :owner, :weight
+   #def initialize(cust_id,weight,date_range=[])
+   #  @owner, @weight = cust_id, weight
+   #end
+   def initialize()
+      print "pawn here\n"
+   end
+end
+
+#class Cat < Pet
+#end
+#class Dog < Pet
+
+
---
+++ check_the_check/run.rb
@@ -0,0 +1,5 @@
+require 'Pawn'
+
+pawn = Pawn.new
+
+
