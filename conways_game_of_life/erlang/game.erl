9b42aa5f768a58eae2dc1c0a9fc13b6a5b6bbab9
Matt Steele <orphum@gmail.com>
2010-11-08 20:59:00 -0600

    version 2 - with tests

--- conways_game_of_life/erlang/game.erl
+++ conways_game_of_life/erlang/game.erl
@@ -1,88 +1,48 @@
 -module(game).
--compile(export_all).
+%%
+%% Exported Functions
+%%
+-export([iterate/1]).
 
-	
+%%
+%% API Functions
+%%
 iterate(World) ->
-	lists:usort(iterate(World, World)).
-	
-iterate([], World) -> [];
-iterate([Cell|T], World) ->
+	getFutureStates(getRelevantCells(World), World).
 
-	TempWorld = processDeadNeighbors(Cell, World),
-	case getFutureLivingState(Cell, World) of
-		Cell -> 
-			[Cell | TempWorld ++ iterate(T, World)];
-		will_die -> 
-			TempWorld ++ iterate(T, World)
+
+%%
+%% Local Functions
+%%	
+getRelevantCells(Cells) ->
+	lists:usort(getRelevantCells_impl(Cells)).
+
+getRelevantCells_impl([]) -> [];
+getRelevantCells_impl([Cell|T]) ->
+	getNeighbors(Cell) ++ getRelevantCells(T).
+
+
+getFutureStates([], _World) -> [];
+getFutureStates([Cell|T], World) ->
+	Alive = lists:member(Cell, World),
+	LivingNeighborCount = getNeighborCount(Cell, World),
+	
+	case {Alive, LivingNeighborCount} of
+		{true, 2} -> [Cell|getFutureStates(T, World)];
+		{_Either, 3} -> [Cell|getFutureStates(T, World)];
+		_Other -> getFutureStates(T, World)
 	end.
 
 
-getFutureLivingState(Cell, World) ->
-	case length(getLivingNeighbors(Cell, World)) of
-		0 -> will_die;
-		1 -> will_die;
-		2 -> Cell;
-		3 -> Cell;
-		_More -> will_die
-	end.
-	
-processDeadNeighbors(LivingCell, World) ->
-	Fn = fun(DeadCell) -> getFutureDeadNeighborState(DeadCell, World) == DeadCell end,
-	lists:filter(Fn, getDeadNeighbors(LivingCell, World)).
-	
-getFutureDeadNeighborState(DeadCell, World) ->
-	case length(getLivingNeighbors(DeadCell, World)) of
-		0 -> still_dead;
-		1 -> still_dead;
-		2 -> still_dead;
-		3 -> DeadCell;
-		_More -> still_dead
-	end.
-	
-getDeadNeighbors(Cell, World) ->
-	getDead(getNeighbors(Cell, World), World).
+getNeighborCount(Cell, World) ->
+	CountFn = fun(C, Count) -> 
+		case lists:member(C, World) of
+			true -> 1 + Count;
+			false -> Count
+		end
+		end,
+	lists:foldl(CountFn, 0, getNeighbors(Cell)).
 
-getLivingNeighbors(Cell, World) ->
-	getLiving(getNeighbors(Cell, World), World).
-
-	
-getNeighbors({X,Y}, World) ->
+getNeighbors({X,Y}) ->
 	[{X,Y-1},{X,Y+1},{X-1,Y-1},{X-1,Y},{X-1,Y+1},{X+1,Y-1},{X+1,Y},{X+1,Y+1}].
-	
-	
-getLiving(Cells, World) ->
-	ExistsFn = fun(Cell) -> lists:member(Cell, World) end,
-	lists:filter(ExistsFn, Cells).
 
-getDead(Cells, World) ->
-	NotExistsFn = fun(Cell) -> not(lists:member(Cell, World)) end,
-	lists:filter(NotExistsFn, Cells).	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-%getLiveNeighbors({X,Y}, [{Lx,Ly}|Tail], N) ->
-%	Yp1 = Y+1,
-%	Ym1 = Y-1,
-%	Xp1 = X+1,
-%	Xm1 = X-1,
-%	NewN = case {Lx,Ly} of
-%		{X,Yp1} -> [{Lx,Ly}|N];
-%		{X,Ym1} -> [{Lx,Ly}|N];
-%		{Xm1, Ym1} -> [{Lx,Ly}|N];
-%		{Xm1, Y} -> [{Lx,Ly}|N];
-%		{Xm1, Yp1} -> [{Lx,Ly}|N];
-%		{Xp1, Ym1} -> [{Lx,Ly}|N];
-%		{Xp1, Y} -> [{Lx,Ly}|N];
-%		{Xp1, Yp1} -> [{Lx,Ly}|N];
-%		_Other -> N
-%	end,
-%	getLiveNeighbors({X,Y}, Tail, NewN).
-	
-	
