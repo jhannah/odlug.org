bfa669078ae400835480d78f5c3425b7b0d97009
Jay Hannah <jhannah@mutationgrid.com>
2011-11-12 16:24:18 -0600

    Better directory name.

--- ants/tools/play_one_game.sh
+++ ants/tools/play_one_game.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 1000 --map_file maps/maze/maze_04p_01.map "$@" "python sample_bots/python/HunterBot.py" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "python sample_bots/python/GreedyBot.py"
+./playgame.py --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 100 --map_file maps/maze/maze_04p_01.map "$@" "python sample_bots/python/HunterBot.py" "python sample_bots/python/LeftyBot.py" "python sample_bots/python/HunterBot.py" "perl ../jhannah_bots/MyBot.pl"
+
