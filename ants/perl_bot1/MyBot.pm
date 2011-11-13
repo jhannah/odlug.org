bb67435fe5798fe87b726e5ee8fecd6fa06ad37f
Jay Hannah <jhannah@mutationgrid.com>
2011-11-12 18:39:59 -0600

    Huh. "If no food head away from other ants" sort of works, but they still do a lot of sitting around.

--- ants/perl_bot1/MyBot.pm
+++ ants/perl_bot1/MyBot.pm
@@ -21,37 +21,62 @@ sub setup {
 # create_orders() will be run after parsing the incoming map data, every turn.
 # This example looks for nearby food that isn't immediately blocked by water.
 sub create_orders {
-    my $self = shift;
+   my $self = shift;
 
-    for my $ant ($self->my_ants) {
-        my @food = $self->nearby_food($ant);
+   for my $ant ($self->my_ants) {
+      my @food = $self->nearby('food', $ant);
 
-        for my $f (@food) {
-            my $as = MyAStar->new($self);
-            my $direction = $as->my_findPath($ant, $f);
+      if (@food) {
+         # Head straight for food using A* algorithm
+         my $f = shift @food;
+         my $as = MyAStar->new($self);
+         my $direction = $as->my_findPath($ant, $f);
+         $self->issue_order( $ant, $direction );
+      } else {
+         # Uhh... head away from the closest other ant?
+         my @ants = $self->nearby('my_ants', $ant);
+         shift @ants;  # throw myself away
+         my $closest = shift @ants;
+         say $log "Avoid other ants!";
+         say $log "I'm at: " .  Dumper($ant);
+         say $log "Closest: " . Dumper($closest);
+         my $direction = $self->direction($closest, $ant);
+         say $log "GO $direction dammit!";
+         if ($self->passable(Position->from($ant)->move($direction))) {
             $self->issue_order( $ant, $direction );
-            last;
-        }
-
-    }
+         } else {
+            say $log "   NO! not passable!";
+         }
+      }
+   }
 }
 
-# This demonstrates the use of some of the methods available in the Ants library.
-# This subroutine returns a list of nearby food for a given location, sorted
-# by distance.
-sub nearby_food {
-    my ($self, $ant_location) = @_;
 
-    my @foods;
-    for my $food ($self->food) {
-        my $dist = $self->distance($ant_location, $food);
-        push @foods, { distance => $dist, tile => $food };
+=head2 nearby
+
+Returns a list of nearby X for a given location, sorted by distance.
+
+   my @nearby_food =       $self->nearby('food',       $location);
+   my @nearby_my_ants =    $self->nearby('my_ants',    $location);
+   my @nearby_enemy_ants = $self->nearby('enemy_ants', $location);
+
+=cut
+
+sub nearby {
+    my ($self, $what, $ant_location) = @_;
+
+    my @rval;
+    for my $loc ($self->$what) {
+        my $dist = $self->distance($ant_location, $loc);
+        push @rval, { distance => $dist, tile => $loc };
     }
 
     # Sort by distance:
     return map { $_->{tile} } sort {
         $a->{distance} <=> $b->{distance}
-    } @foods;
+    } @rval;
 }
+
+
 
 1;
