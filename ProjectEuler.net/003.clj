dbe8b758553eaae66571d288d294a6ec300c4f4b
Juan Vazquez <juanvazquez@gmail.com>
2012-09-15 19:14:23 -0500

    adding 003.clj

---
+++ ProjectEuler.net/003.clj
@@ -0,0 +1,13 @@
+;;; Juan Vazquez 
+;;; https://github.com/javazquez
+;;; http://javazquez.com
+
+(defn prime? [num]
+    (if (< (count (filter #(zero? (rem num %))(range 1 num ))) 3)
+    true
+    false))
+
+(defn factors [num](filter #(zero? (rem num %)) (range 2 (Math/sqrt num) )))
+
+(reduce max (filter #(prime? %) (factors 600851475143)))
+
