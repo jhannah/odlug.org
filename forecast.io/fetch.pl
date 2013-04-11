f22440c42ca6a5ace616f99bdf7abecb704205ff
Jay Hannah <jhannah@mutationgrid.com>
2013-04-11 18:57:23 -0500

    Re-org for multiple locations on the same page.

--- forecast.io/fetch.pl
+++ forecast.io/fetch.pl
@@ -15,58 +15,62 @@ my @jsonkeys = qw(
    temperatureMin temperatureMax
 );
 
-open my $out, ">:utf8", "thurman_ia.html";
+open my $out, ">:utf8", "out.html";
 print $out <<EOT;
-<h1>Thurman, IA History</h1>
-
 <p>
 <b>precipIntensity</b>: inches of liquid water per hour<br/>
 <b>precipAccumulation</b>: inches snow accum<br/>
 <a href="https://developer.forecast.io/docs/v2">Read more</a>
 </p>
-
-<table>
-<tr>
-   <th>Date</th>
-EOT
-foreach my $key (@jsonkeys) {
-   say $out "   <th>$key</th>";
-}
-say $out <<EOT;
-   <th> </th>
-</tr>
 EOT
 
-foreach my $daysago (0..7) {         # For the last week
-   my $time = time - 86400 * $daysago;
-   my $date = date $time;
-   my $uri = sprintf(
-      "https://api.forecast.io/forecast/%s/%s,%s?exclude=currently,minutely,hourly,flags",
-      $api_key,
-      $latlong,
-      $time,
-   );
-   # say $uri;
-   $mech->get($uri);
-   # say $mech->content;
-   my $json = (decode_json $mech->content)->{daily}->{data}->[0];
-   print $out "<tr><td>";
-   print $out join "</td><td>", 
-      $date,
-      @$json{ @jsonkeys };
-   my $weburl = "http://forecast.io/#/f/$latlong/$time";
-   print $out "<td><a href='$weburl' target='_blank'>web</a>";
-   say $out "</td></tr>";
-}
+location("Thurman, IA", '40.8198,-95.7521');  # Where Jay dirt bikes
+location("Omaha, NE",   '41.2607,-95.9406');  # Where Jay lives (for comparison)
 
 say $out <<EOT;
-
-</table>
-
 <br/><br/><br/><br/>
 Data from <a href="https://developer.forecast.io/">forecast.io API</a><br/>
 <a href="https://github.com/jhannah/odlug.org/tree/master/forecast.io">soure code</a>
 EOT
+
+
+sub location {
+   my ($name, $latlong) = @_;
+    print $out <<EOT;
+<h1>$name</h1>
+<table>
+<tr>
+   <th>Date</th>
+EOT
+   foreach my $key (@jsonkeys) {
+      say $out "   <th>$key</th>";
+   }
+   say $out "<th> </th>\n</tr>";
+   
+   foreach my $daysago (0..7) {         # For the last week
+      my $time = time - 86400 * $daysago;
+      my $date = date $time;
+      my $uri = sprintf(
+         "https://api.forecast.io/forecast/%s/%s,%s?exclude=currently,minutely,hourly,flags",
+         $api_key,
+         $latlong,
+         $time,
+      );
+      # say $uri;
+      $mech->get($uri);
+      # say $mech->content;
+      my $json = (decode_json $mech->content)->{daily}->{data}->[0];
+      print $out "<tr><td>";
+      print $out join "</td><td>", 
+         $date,
+         @$json{ @jsonkeys };
+      my $weburl = "http://forecast.io/#/f/$latlong/$time";
+      print $out "<td><a href='$weburl' target='_blank'>web</a>";
+      say $out "</td></tr>";
+   }
+
+   say $out "</table>";
+}
 
 
 
